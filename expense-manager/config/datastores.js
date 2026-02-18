d3d688f7df9e2bed39e055534906cadea76eb3ea
Jay Thummar <jayt@zignuts.com>
2026-02-18 14:00:22 +0530

    `Added.DS_Store file`

--- expense-manager/config/datastores.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports.datastores = {
-  default: {
-    adapter: 'sails-mongo',
-    url: process.env.MONGO_URI,
-  },
-};
--- expense-manager/config/routes.js
+++
@@ -1,6 +0,0 @@
-module.exports.routes = {
-  'GET /signup': { view: 'signup' },
-  'POST /signup': 'AuthController.signup',
-  'GET /login': { view: 'login' },
-  'POST /login': 'AuthController.login',
-};
